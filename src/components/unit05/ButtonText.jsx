c0a1f62c9f17918eb91cb31b0daf8bc30530afb3
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-12 16:08:23 +0900

    change: Change ButtonText.css into ButtonText.module.scss

--- src/components/unit05/ButtonText.jsx
+++ src/components/unit05/ButtonText.jsx
@@ -1,12 +1,25 @@
-import './ButtonText.css'
+import classes from "./ButtonText.module.scss";
+// import "./ButtonText.css";
+
+// jsでクラスを定義
+// そのスコープが対象コンポーネントの外に出る事はない
+const buttonStyle = {
+  display: "flex",
+  justifyContent: "spaceBetween",
+  alignItems: "center",
+  padding: "20px 60px",
+  backgroundColor: "skyblue",
+  borderRadius: "24px",
+  columnGap: "12px",
+};
 
 const ButtonText = (props) => {
-    return  (
-        <div className="ButtonWrapper">
-            <h1 className="ButtonTitle">{props.title}</h1>
-            <h2 className="ButtonContent">{props.content}</h2>
-        </div>
-    );
+  return (
+    <div style={buttonStyle} className="ButtonWrapper">
+      <h1 className={classes.ButtonTitle}>{props.title}</h1>
+      <h2 className={classes.ButtonContent}>{props.content}</h2>
+    </div>
+  );
 };
 
 export default ButtonText;
