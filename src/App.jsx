a1cfa1e6ed9645b95e03fb0316714eaf738bfd31
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-06-27 21:45:56 +0900

    add: <p>MODE: {import.meta.env.MODE}</p>

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,14 @@ import classes from "./App.module.scss";
 function App() {
   return (
     <div className={classes.App}>
+      <p>MODE: {import.meta.env.MODE}</p>
       <h1>Vite + React</h1>
       <h2 className={classes.heading}>unit09</h2>
       <Fetch />
       <h2 className={classes.heading}>unit08</h2>
       <h3 className={classes.subheading}>Counter</h3>
       <ToggleButton08 />
-      {/* <h3 className={classes.subheading}>Counter</h3>
+      <h3 className={classes.subheading}>Counter</h3>
       <Counter08 />
       <h2 className={classes.heading}>unit07</h2>
       <h3 className={classes.subheading}>Toggle</h3>
@@ -31,7 +32,7 @@ function App() {
       <h2 className={classes.heading}>unit06</h2>
       <Article title={"export & import を練習した日"} content={"2023/04/17"} />
       <h2 className={classes.heading}>unit05</h2>
-      <Button /> */}
+      <Button />
       <h2 className={classes.heading}>gsap</h2>
       <Box />
     </div>
